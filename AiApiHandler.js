70d1121f7c845558effe2eb69bdd20b9e0e392d8
unknown <koala34025@gmail.com>
2024-10-10 20:01:51 +0800

    Now can talk to python backend

--- AiApiHandler.js
+++ AiApiHandler.js
@@ -1,14 +1,20 @@
 export const simulateAiOverlay = async () => {
-  // Simulate a delay for the AI overlay generation
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  const fetchRandomNum = async () => {
+    try {
+      const response = await fetch('http://192.168.0.101:5000/random-number');
+      const data = await response.json();
+      const images = [
+        require('./assets/pose/1.png'),
+        require('./assets/pose/2.png'),
+        require('./assets/pose/3.png'),
+      ];
+      const randomImage = images[data.random_number];
+      return randomImage;
+    } catch (error) {
+      console.error('Error fetching random number:', error);
+      throw error; // Re-throw the error to handle it in the calling function
+    }
+  };
 
-  const images = [
-    require('./assets/pose/1.png'),
-    require('./assets/pose/2.png'),
-    require('./assets/pose/3.png'),
-  ];
-  const randomImage = images[Math.floor(Math.random() * images.length)];
-
-  // Simulate the AI overlay image (in a real scenario, this would be returned by the API)
-  return randomImage;
-}
+  return await fetchRandomNum();
+};
